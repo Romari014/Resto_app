99ca198f5edb198bbf6b7bc0277dc87ef8df804a
Roman <roma00062@gmail.com>
2021-08-26 22:50:28 +0200

    Organized of work with the server

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import cartIcon from './shopping-cart-solid.svg';
-import { Link } from 'react-router-dom';
 import './app-header.scss';
+import { Link } from 'react-router-dom';
 
 const AppHeader = ({total}) => {
     return (
         <header className="header">
-            <Link to={'/'} className="header__link" >Menu</Link>
-            <Link to= '/cart' className="header__link">
+            <Link to ={'/'} className = "header__link">Menu</Link>
+            <Link to = "/cart" className = "header__link">
                 <img className="header__cart" src={cartIcon} alt="cart"></img>
                 Total: {total} $
             </Link>
--- src/services/resto-service.js
+++ src/services/resto-service.js
@@ -1,14 +1,26 @@
-export default class RestoService  {
+  
+export default class RestoService{
+    _apiBase = 'http://localhost:3000';
 
-    url = 'http://localhost:3000/menu';
-
-    getMenuItems =  async () => {
-        const response = await fetch(this.url);
-
-        if(!response.ok) {
-            throw new Error('Server error');
+    async getResource(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}` + 
+                `, received ${res.status}`);
         }
-        const result = await response.json();
-        return result;
+        return await res.json();
     }
+
+    async getMenuItems () {
+        return await this.getResource('/menu/');
+    }
+
+    async getItem(id) {
+        const res = await this.getResource('/menu/');
+        const item = res.find( (el) => {
+            console.log(`el.id: ${el.id}, id: ${id}`);
+            return el.id === +id;
+        }) 
+        return item;
+    } 
 }

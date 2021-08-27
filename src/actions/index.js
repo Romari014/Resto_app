62f25c3b3c3fc1d1e162dca2ccf988b5c654cb23
Roman <roma00062@gmail.com>
2021-08-27 13:57:30 +0200

    Refactored code and add new features

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,15 @@
-const  menuLoaded = (newMenu) => {
+const menuLoaded = (newMenu) => {
     return {
         type: 'MENU_LOADED',
         payload: newMenu
-    };
-};
+    }
+}
 
-const  menuRequested = () => {
+const menuRequested = () => {
     return {
         type: 'MENU_REQUESTED',
-    };
-};
+    }
+}
 
 const menuError = () => {
     return {
@@ -17,8 +17,24 @@ const menuError = () => {
     }
 }
 
+const addedToCart = (id) => {
+    return {
+        type: 'ITEM_ADD_TO_CART',
+        payload: id
+    }
+}
+
+const deleteFromCart = (id) => {
+    return {
+        type: 'ITEM_REMOVE_FROM_CART',
+        payload: id
+    }
+}
+
 export {
     menuLoaded,
     menuRequested,
-    menuError
+    menuError, 
+    addedToCart, 
+    deleteFromCart
 };
